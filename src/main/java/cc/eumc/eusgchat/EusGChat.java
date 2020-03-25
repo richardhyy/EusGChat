31057a1aa76c07d3006655fef7df2edd2d2d0e09
AlanRichard <alan_richard@eumc.cc>
2020-03-25 19:09:57 +0800

    Add: ChatBridge: webhook support Add: GlobalGhostMode: hide public messages to console

--- src/main/java/cc/eumc/eusgchat/EusGChat.java
+++ src/main/java/cc/eumc/eusgchat/EusGChat.java
@@ -1,6 +1,5 @@
 package cc.eumc.eusgchat;
 
-import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
@@ -13,17 +12,16 @@ import net.md_5.bungee.config.ConfigurationProvider;
 import net.md_5.bungee.config.YamlConfiguration;
 import net.md_5.bungee.event.EventHandler;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import javax.net.ssl.HttpsURLConnection;
+import java.io.*;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public final class EusGChat extends Plugin implements Listener {
-    String CHAT_FORMAT_SERVER_INDICATOR;
-    String CHAT_FORMAT_MESSAGE;
-    List<String> BYPASS_SERVERS = new ArrayList<>();
+    Configuration config;
 
     @Override
     public void onEnable() {
@@ -39,17 +37,17 @@ public final class EusGChat extends Plugin implements Listener {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
+            try (InputStream in = getResourceAsStream("webhook_example.php")) {
+                Files.copy(in, file.toPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
         try {
-            Configuration config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(file);
-
-            CHAT_FORMAT_SERVER_INDICATOR = config.getString("Settings.ChatFormat.ServerIndicator", "&7{server} ").replace("&", "§");
-            CHAT_FORMAT_MESSAGE = "§r" + config.getString("Settings.ChatFormat.Message", "{player} &f>&7>&8> &r{message}").replace("&", "§");
-
-            BYPASS_SERVERS = config.getStringList("Settings.BypassServer");
-
-            getLogger().info("Bypass servers: " + BYPASS_SERVERS);
+            config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(file);
+            new PluginConfig(this);
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -70,19 +68,86 @@ public final class EusGChat extends Plugin implements Listener {
 
         ProxiedPlayer player = (ProxiedPlayer) e.getSender();
 
-        if (BYPASS_SERVERS.contains(player.getServer().getInfo().getName())) {
+        if (PluginConfig.BYPASS_SERVERS.contains(player.getServer().getInfo().getName())) {
             return;
         }
 
         TextComponent base = new TextComponent();
 
-        TextComponent prefix = new TextComponent(CHAT_FORMAT_SERVER_INDICATOR.replace("{server}", String.valueOf(player.getServer().getInfo().getName().charAt(0)).toUpperCase()));
+        TextComponent prefix = new TextComponent(PluginConfig.CHAT_FORMAT_SERVER_INDICATOR.replace("{server}", String.valueOf(player.getServer().getInfo().getName().charAt(0)).toUpperCase()));
         prefix.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(player.getServer().getInfo().getName()).create()));
         base.addExtra(prefix);
 
-        base.addExtra(new TextComponent(CHAT_FORMAT_MESSAGE.replace("{player}", player.getDisplayName()).replace("{message}", e.getMessage())));
+        base.addExtra(new TextComponent(PluginConfig.CHAT_FORMAT_MESSAGE.replace("{player}", player.getDisplayName()).replace("{message}", e.getMessage())));
 
-        getProxy().broadcast(base);
+        if (PluginConfig.GlobalGhostMode) {
+            for (ProxiedPlayer p : getProxy().getPlayers()) {
+                p.sendMessage(base);
+            }
+        }
+        else {
+            getProxy().broadcast(base);
+        }
+
+        if (PluginConfig.Sending_Enabled) {
+            getProxy().getScheduler().schedule(this, () -> {
+                for (String webhookURL : PluginConfig.Sending_Webhooks) {
+                    sendMessageToWebhook(webhookURL, player.getName(), e.getMessage());
+                }
+            }, 1, TimeUnit.MILLISECONDS);
+        }
+
         e.setCancelled(true);
+    }
+
+    public Configuration getConfig() {
+        return config;
+    }
+
+    void sendMessageToWebhook(String webhookURL, String playerName, String message) {
+        try {
+            String params = "";
+            params = "password=" + URLEncoder.encode(PluginConfig.Sending_ConnectionPassword, StandardCharsets.UTF_8.toString());
+            params += "&playername=" + URLEncoder.encode(playerName, StandardCharsets.UTF_8.toString());
+            params += "&message=" + URLEncoder.encode(message, StandardCharsets.UTF_8.toString());
+            httpPost(webhookURL, params);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static String httpPost(String url, String urlParameters) throws Exception {
+        try {
+            URL obj = new URL(url);
+            HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+            //add reuqest header
+            con.setRequestMethod("POST");
+
+            // Send post request
+            con.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.writeBytes(urlParameters);
+            wr.flush();
+            wr.close();
+
+            int responseCode = con.getResponseCode();
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            //print result
+            return response.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
---
+++ src/main/java/cc/eumc/eusgchat/PluginConfig.java
@@ -0,0 +1,34 @@
+package cc.eumc.eusgchat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PluginConfig {
+    public static boolean Sending_Enabled;
+    public static String Sending_ConnectionPassword;
+    public static List<String> Sending_Webhooks;
+
+    public static boolean GlobalGhostMode = false;
+    public static String CHAT_FORMAT_SERVER_INDICATOR;
+    public static String CHAT_FORMAT_MESSAGE;
+    public static List<String> BYPASS_SERVERS = new ArrayList<>();
+
+    public PluginConfig(EusGChat instance) {
+        Sending_Enabled = instance.getConfig().getBoolean("Settings.ChatBridge.Enabled", false);
+        if (Sending_Enabled) {
+            Sending_ConnectionPassword = instance.getConfig().getString("Settings.ChatBridge.ConnectionPassword", "");
+            Sending_Webhooks = instance.getConfig().getStringList("Settings.ChatBridge.Webhooks");
+        }
+        instance.getLogger().info("ChatBridge: " + (Sending_Enabled?"§aenabled":"disabled"));
+
+
+        GlobalGhostMode = instance.getConfig().getBoolean("Settings.GlobalGhostMode", false);
+        instance.getLogger().info("GlobalGhostMode: " + (GlobalGhostMode?"§aenabled":"disabled"));
+
+        CHAT_FORMAT_SERVER_INDICATOR = instance.getConfig().getString("Settings.ChatFormat.ServerIndicator", "&7{server} ").replace("&", "§");
+        CHAT_FORMAT_MESSAGE = "§r" + instance.getConfig().getString("Settings.ChatFormat.Message", "{player} &f>&7>&8> &r{message}").replace("&", "§");
+
+        BYPASS_SERVERS = instance.getConfig().getStringList("Settings.BypassServer");
+        instance.getLogger().info("Bypass servers: " + BYPASS_SERVERS);
+    }
+}
